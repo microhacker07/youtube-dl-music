de58d4fe21caf4cf646c2aa2e5916477c4b65ad6
microhacker <microhacker07@users.noreply.github.com>
2021-10-13 22:51:08 -0400

    fixed text bug

--- download-playlist.sh
+++ download-playlist.sh
@@ -2,7 +2,7 @@
 
 #youtube-dl --download-archive downloaded.txt --no-post-overwrites --write-thumbnail --add-metadata -ciwx --audio-format mp3 -o "./%(playlist)s/%(title)s-%(id)s.%(ext)s" -a playlists.txt
 
-youtube-dl --cookies ./cookies.txt --download-archive downloaded.txt --no-post-overwrites -f bestaudio[ext=m4a] --extract-audio --add-metadata --write-thumbnail -o "./music/%(title)s.%(ext)s - %(id)s" -a playlists.txt
+youtube-dl --cookies ./cookies.txt --download-archive downloaded.txt --no-post-overwrites -f bestaudio[ext=m4a] --extract-audio --add-metadata --write-thumbnail -o "./music/%(title)s - %(id)s.%(ext)s" -a playlists.txt
 
 # From https://gist.github.com/tabrindle/ed9f77b4e96f4c98b49b
 
