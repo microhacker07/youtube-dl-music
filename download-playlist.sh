21fa5756b9540e8f66f1af39d50232a60369fcd9
microhacker <microhacker07@users.noreply.github.com>
2021-10-14 20:19:48 -0400

    added retry while loop + gitignore

--- download-playlist.sh
+++ download-playlist.sh
@@ -2,7 +2,24 @@
 
 #youtube-dl --download-archive downloaded.txt --no-post-overwrites --write-thumbnail --add-metadata -ciwx --audio-format mp3 -o "./%(playlist)s/%(title)s-%(id)s.%(ext)s" -a playlists.txt
 
-youtube-dl --cookies ./cookies.txt --download-archive downloaded.txt --no-post-overwrites -f bestaudio[ext=m4a] --extract-audio --add-metadata --write-thumbnail -o "./music/%(title)s - %(id)s.%(ext)s" -a playlists.txt
+#youtube-dl --cookies ./cookies.txt --download-archive downloaded.txt --no-post-overwrites -f bestaudio[ext=m4a] --extract-audio --add-metadata --write-thumbnail -o "./music/%(title)s - %(id)s.%(ext)s" -a playlists.txt
+
+FILENAME_OUTPUT="./music/%(title)s - %(id)s.%(ext)s"
+COOKIES="--cookies ./cookies.txt"
+OPTIONS="${COOKIES} --download-archive downloaded.txt --no-post-overwrites -f bestaudio[ext=m4a] --extract-audio --add-metadata"
+
+echo -e "Starting download of all playlists in ./playlists.txt\n"
+STATUS=1
+
+while [ "$STATUS" -eq "1" ]
+do
+    youtube-dl $OPTIONS -o "${FILENAME_OUTPUT}" -a playlists.txt
+    STATUS=$?
+    if [ "$STATUS" -eq "1" ]
+    then
+        echo -e "\nRestarting the download -----\n";
+    fi
+done
 
 # From https://gist.github.com/tabrindle/ed9f77b4e96f4c98b49b
 
