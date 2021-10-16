7cb936f736b217d1e987c96f8fb29bc6fd931fbc
microhacker <microhacker07@users.noreply.github.com>
2021-10-16 01:03:09 -0400

    found something called beets and will reformat around that

--- download-playlist.sh
+++ download-playlist.sh
@@ -4,22 +4,13 @@
 
 #youtube-dl --cookies ./cookies.txt --download-archive downloaded.txt --no-post-overwrites -f bestaudio[ext=m4a] --extract-audio --add-metadata --write-thumbnail -o "./music/%(title)s - %(id)s.%(ext)s" -a playlists.txt
 
-FILENAME_OUTPUT="./music/%(title)s - %(id)s.%(ext)s"
+FILENAME_OUTPUT="./unidentified-music/%(title)s - %(id)s.%(ext)s"
 COOKIES="--cookies ./cookies.txt"
 OPTIONS="${COOKIES} --download-archive downloaded.txt --no-post-overwrites -f bestaudio[ext=m4a] --extract-audio --add-metadata"
 
 echo -e "Starting download of all playlists in ./playlists.txt\n"
-STATUS=1
 
-while [ "$STATUS" -eq "1" ]
-do
-    youtube-dl $OPTIONS -o "${FILENAME_OUTPUT}" -a playlists.txt
-    STATUS=$?
-    if [ "$STATUS" -eq "1" ]
-    then
-        echo -e "\nRestarting the download -----\n";
-    fi
-done
+yt-dlp $OPTIONS -o "${FILENAME_OUTPUT}" -a playlists.txt
 
 # From https://gist.github.com/tabrindle/ed9f77b4e96f4c98b49b
 
